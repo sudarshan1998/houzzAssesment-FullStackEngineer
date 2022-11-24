56aa074fc93f680d8d1a280114a8c0b98356d059
sudarshan1998 <sudarshandhakal21@gmail.com>
2022-11-24 03:26:10 -0800

    update: clean code

--- my-app/src/components/addNewBeerModal/index.js
+++ my-app/src/components/addNewBeerModal/index.js
@@ -3,13 +3,49 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import "./index.css"
-import image from "../../types/houzz.png"
+import image from "../../images/houzz.png"
 
 function AddNewBeer() {
-  const [show, setShow] = useState(false);
+  const [enteredName, setEnteredName] = useState("")
+  const [enteredGenre, setEnteredGenre] = useState("")
+  const [enteredDescription, setEnteredDescription] = useState("")
 
+  const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const[enteredNameIsValid, setEnteredNameIsValid] = useState(false)
+  const [enteredGenreIsValid, setEnteredGenreIsValid] = useState(false)
+  const [enteredDescriptionIsValid, setEnteredDescriptionIsValid] = useState(false)
+
+  const inputNameHandler = event => {
+    setEnteredName(event.target.value)
+  }
+
+  const inputGenreHandler = event => {
+    setEnteredGenre(event.target.value)
+  }
+  const inputDescriptionHandler = event => {
+    setEnteredDescription(event.target.value)
+  }
+
+  const formSubmitHandler = event => {
+    event.preventDefault()
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false)
+      return
+    }
+    if (enteredGenre.trim() === "") {
+      setEnteredGenreIsValid(false)
+      return
+    }
+    if (enteredDescription.trim() === "") {
+      setEnteredDescriptionIsValid(false)
+      return
+    }
+    setEnteredNameIsValid(true)
+    setEnteredGenreIsValid(true)
+    setEnteredDescriptionIsValid(true)
+  }
 
   return (
     <>
@@ -23,37 +59,49 @@ function AddNewBeer() {
         </Modal.Header>
         <Modal.Body>
           <img className="beerImage" src={image}/>
-          <Form>
+          <Form onSubmit={formSubmitHandler}>
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Beer name</Form.Label>
               <Form.Control
-                type="string"
+                onChange={inputNameHandler}
+                type="text"
                 placeholder="Beer name*"
+                value={enteredName}
                 autoFocus
               />
+              {!enteredNameIsValid && <p className='error-text'>Beer name must not be empty.</p>}
+            </Form.Group>
+            <Form.Group>
               <Form.Label>Genre</Form.Label>
               <Form.Control
-                type="string"
+                onChange={inputGenreHandler}
+                type="text"
                 placeholder="Genre*"
+                value={enteredGenre}
               />
+              {!enteredGenreIsValid && <p className="error-text">Beer genre must not be empty.</p>}
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label>Description*</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+            <Form.Label>Description*</Form.Label>
+              <Form.Control 
+                onChange={inputDescriptionHandler}
+                as="textarea" 
+                value={enteredDescription}
+                rows={3} 
+              />
+              {!enteredDescriptionIsValid && <p className="error-text">Beer description must not be empty.</p>}
             </Form.Group>
+            <Button variant="primary" className='saveButton' type = "submit" onClick={handleClose}>
+              Save
+            </Button>
+            <Button variant="danger" className='cancelButton' onClick={handleClose}>
+              Cancel
+            </Button>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="danger" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
--- my-app/src/components/beerItem/index.js
+++ my-app/src/components/beerItem/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { CCardBody, CCardImage, CRow, CCol, CCardTitle, CCardText, CCard } from '@coreui/react'
 import ReactTooltip from "react-tooltip";
+import "./index.css"
 
 const BeerItem = (props) => {
   return (
       <CCard className="mb-3">
-        <CRow className="g-0 parent">
+        <CRow className="g-0 parent hoverCard">
           <CCol md={2} className="text-center">
             <ReactTooltip place="top" type="dark" effect="solid"></ReactTooltip>
             <CCardImage src={props.image} style={{width: "auto", maxHeight: "200px", margin: "16px 16px"}} data-tip={"ingredients: " + props.ingredients}/>
